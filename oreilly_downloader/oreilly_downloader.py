d0a8501afb9ba096f451c84bbf359093bc8e728d
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2017-12-10 17:36:59 +0100

    Reorders imports.

--- oreilly_downloader/oreilly_downloader.py
+++ oreilly_downloader/oreilly_downloader.py
@@ -8,9 +8,9 @@ Author: Alberto Martinez de Murga (@threkk)
 License: MIT
 """
 
+from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
-from __future__ import absolute_import
 
 import argparse
 import re
